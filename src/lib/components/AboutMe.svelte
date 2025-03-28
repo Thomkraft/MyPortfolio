d5abe9caf76d92acbd685be01396f47f392601ae
thomas koenig <thomas.koenig@etu.univ-lyon1.fr>
2025-03-28 15:05:19 +0100

    feat:Download CV button

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -1,9 +1,28 @@
 <script>
-    import { Hr, Heading, Span, P, GradientButton } from 'flowbite-svelte';
+	import { goto } from '$app/navigation';
+    import { Hr, Heading, Span, P, GradientButton,Spinner } from 'flowbite-svelte';
     import { AddressBookSolid, MessageCaptionSolid } from 'flowbite-svelte-icons';
+
+    let isLoading = false;
+
+    function DownloadCV() {
+        isLoading = true;
+
+        // Attendre un court instant pour simuler un chargement
+        setTimeout(() => {
+            const link = document.createElement('a');
+            link.href = '/cv-koenig-thomas.pdf';
+            link.download = 'Thomas_Koenig_CV.pdf'; 
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            isLoading = false;
+        }, 1500); // Simule un court délai pour l'effet loading
+    }
 </script>
 
-<div class="AboutMe grid grid-cols-1 md:grid-cols-2 gap-10 items-center mx-15">
+<div class="AboutMe grid grid-cols-1 md:grid-cols-2 gap-10 items-center mx-15 justify-center">
     
     <!-- Colonne Gauche : Texte -->
     <div class="AboutMeText">
@@ -13,7 +32,7 @@
         
         <Hr classHr="w-95% h-1 my-4 rounded-sm mb-5 dark:bg-orange bg-orange" />
         
-        <div class="Description w-50% ml-5 text-justify">
+        <div class="Description w-50% ml-5 mr-5 text-justify">
             <P>
                 Post haec indumentum regale quaerebatur et ministris fucandae purpurae tortis confessisque pectoralem tuniculam sine manicis textam, 
                 Maras nomine quidam inductus est ut appellant Christiani diaconus, 
@@ -25,8 +44,14 @@
 
     <!-- Colonne Droite : Boutons -->
     <div class="Buttons flex flex-col gap-5 justify-center items-center">
-        <GradientButton shadow color="blue" size="xl"><AddressBookSolid class="mr-2" /> Télécharger mon CV</GradientButton>
-        <GradientButton shadow color="blue" size="xl"><MessageCaptionSolid class="mr-2"/>Me contacter</GradientButton>
+        <GradientButton on:click={DownloadCV} shadow color="blue" size="xl"> 
+            {#if isLoading}
+                <Spinner class="me-3" size="4" color="white"/>Loading ... 
+            {:else }
+                <AddressBookSolid class="mr-2" /> Télécharger mon CV
+            {/if}
+        </GradientButton>
+        <GradientButton href="/" shadow color="blue" size="xl"><MessageCaptionSolid class="mr-2"/>Me contacter</GradientButton>
     </div>
 
 </div>
