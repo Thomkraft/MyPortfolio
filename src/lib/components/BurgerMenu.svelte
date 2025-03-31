c3f53041e189a9cb7c92e76ebad9cee1ce20360a
thomas koenig <thomas.koenig@etu.univ-lyon1.fr>
2025-03-31 13:34:29 +0200

    feat:Init Burger menu at th ebottom of the screen to DL CV

---
+++ src/lib/components/BurgerMenu.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from "svelte";
+	import DownloadButton from "./UtilsButton.svelte";
+	import UtilsButton from "./UtilsButton.svelte";
+    
+    let isOpen = false;
+
+    function toggleMenu() {
+        isOpen = !isOpen;
+    }
+</script>
+
+
+<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    
+    <!-- Bouton Menu Hamburger -->
+    <button 
+        on:click={toggleMenu} 
+        class="fixed bottom-4 right-4 z-50 inline-flex items-center justify-center p-2 w-12 h-12 text-sm text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+
+    <!-- Menu Hamburger -->
+    <div class={`mb-2 fixed bottom-16 right-4 w-65 rounded-lg shadow-lg bg-white dark:bg-gray-800 transition-transform duration-300 ${isOpen ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}>
+        <ul class="flex flex-col font-medium p-3 space-y-2">
+            <UtilsButton />
+        </ul>
+    </div>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import AboutMe from '$lib/components/AboutMe.svelte';
+import AboutMe from '$lib/components/AboutMe.svelte';
 import NavBar from '$lib/components/NavBar.svelte';
 import { Alert } from 'flowbite-svelte';
 import { DarkMode } from 'flowbite-svelte';
@@ -7,29 +7,66 @@ import { Hr,Heading,Span,P } from 'flowbite-svelte';
 
 import FadeIn from '$lib/components/FadeIn.svelte';
 
-  </script>
+
+import { onMount } from 'svelte';
+import BurgerMenu from '$lib/components/BurgerMenu.svelte';
+  
+let isMobile = false;
+
+//detect screen size
+const checkScreenSize = () => {
+isMobile = window.innerWidth < 1024; 
+};
+
+// Initial check and update if screen size changes
+onMount(() => {
+checkScreenSize();
+window.addEventListener('resize', checkScreenSize); 
+
+return () => {
+    window.removeEventListener('resize', checkScreenSize);
+};
+});
+
+</script>
 
 
 <NavBar />
 
 
-<main class="min-h-screen h-[200vh] bg-opacity-60 mt-30">
+<main class="h-screen bg-opacity-60 mt-40 flex flex-col items-start lg:flex-row">
+    
+    {#if !isMobile }
+        <div class="image flex justify-center items-center w-1/4 h-2/3">
+            <img class="w-3/4 max-h-[80%]" src="/blank-profile.png" alt="Picture_of_me">
+        </div>
+    {/if}
+    
 
+    <div class="flex flex-col w-3/4 justify-start gap-20">
+        
+        <div class="title flex flex-col items-center text-center h-fit mb-10">
+            <Heading tag="h1" class="mb-1" customSize="text-3xl font-bold md:text-4xl lg:text-5xl">
+                <Span>Thomas Koenig</Span>
+            </Heading>
+            <Heading tag="h2" class="mb-1" customSize="text-2xl font-bold md:text-2xl lg:text-3xl">
+                <Span>Étudiant en BUT informatique</Span>
+            </Heading>
+        </div>
 
-    <div class="title flex flex-col items-center text-center justify-center mt-20 mb-20">
-        <Heading tag="h1" class="mb-4" customSize="text-3xl font-bold  md:text-4xl lg:text-5xl">
-            <Span >Thomas Koenig</Span>
-        </Heading>
+        {#if isMobile}
+            <div class="image flex justify-center items-center w-1/4 h-2/3">
+                <img class="w-3/4 max-h-[80%]" src="/blank-profile.png" alt="Picture_of_me">
+            </div>
+        {/if}
 
-        <Heading tag="h2" class="mb-4" customSize="text-2xl font-bold  md:text-2xl lg:text-3xl ">
-            <Span>Étudiant en BUT informatique</Span>
-        </Heading>
-
+        <div class="flex justify-center self-start">
+            <AboutMe />
+        </div>
+        
+        <BurgerMenu />
     </div>
 
-    <AboutMe />
-
+    
 
 </main>
-
-  
