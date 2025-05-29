f14df59edd6fdce0f9b570d74e5831753ae8371f
thomas koenig <thomas.koenig@etu.univ-lyon1.fr>
2025-05-29 15:51:08 +0200

    Add Project Card and modals for project cards

--- src/lib/components/BurgerMenu.svelte
+++ src/lib/components/BurgerMenu.svelte
@@ -1,47 +1,38 @@
 <script>
     import { onMount } from "svelte";
-	import DownloadButton from "./UtilsButton.svelte";
-	import UtilsButton from "./UtilsButton.svelte";
-    import { Button } from 'flowbite-svelte';
-    import { ThumbsUpSolid, ArrowUpOutline } from 'flowbite-svelte-icons';
-    
-    let isOpen = false;
+    import { ArrowUpOutline, DownloadSolid } from 'flowbite-svelte-icons';
+    import { fade } from "svelte/transition";
 
-    function toggleMenu() {
-        isOpen = !isOpen;
-    }
+    let isOpen = false;
 
     function closeMenu() {
         isOpen = false;
     }
 
+    onMount(() => {
+    window.addEventListener('scroll', () => {
+        closeMenu(); // Ferme le menu si l'utilisateur fait défiler la page
+    });
+    });
+
 </script>
 
-<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4" in:fade={{ duration: 250 }} out:fade={{ duration: 250 }}>
 
-
-    <!-- Bouton Menu Hamburger -->
-    <button 
-        on:click={toggleMenu} 
-        class="fixed bottom-20 right-4 z-50 inline-flex items-center justify-center p-2 w-12 h-12 text-sm text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 18">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 1v11m0 0 4-4m-4 4L4 8m11 4v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3"/>
-        </svg>
-    </button>
-
-    <!-- Menu Hamburger -->
-    <div class={`mb-2 fixed bottom-35 right-4 w-65 rounded-lg shadow-lg bg-white dark:bg-gray-800 transition-transform duration-300 ${isOpen ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}>
-        <ul class="flex flex-col font-medium p-3 space-y-2">
-            <UtilsButton onComplete={closeMenu} />
-        </ul>
-    </div>
+    <!-- Bouton pour télécharger le CV  -->
+    <a 
+    href="/cv-koenig-thomas.pdf" 
+    download="cv-koenig-thomas.pdf"
+    class="fixed bottom-23 right-4 z-50 p-2 w-15 h-15 text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all"
+    >
+        <DownloadSolid class="w-11 h-11 text-gray-800 dark:text-white" />
+    </a>
 
     <!-- Bouton pour remonter -->
     <button 
-        class="fixed bottom-4 right-4 z-50 p-2 w-12 h-12 text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all"
+        class="fixed bottom-4 right-4 z-50 p-2 w-15 h-15 text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all"
         on:click={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-        <ArrowUpOutline class="w-8 h-8 text-gray-800 dark:text-white" />
+        <ArrowUpOutline class="w-11 h-11 text-gray-800 dark:text-white" />
     </button>
 
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,15 @@
 <script>
 import AboutMe from '$lib/components/AboutMe.svelte';
 import NavBar from '$lib/components/NavBar.svelte';
-import { Alert } from 'flowbite-svelte';
-import { DarkMode } from 'flowbite-svelte';
-import { Hr,Heading,Span,P } from 'flowbite-svelte';
-
-import FadeIn from '$lib/components/FadeIn.svelte';
+import { Hr,Heading,Span } from 'flowbite-svelte';
 
 
 import { onMount } from 'svelte';
 import BurgerMenu from '$lib/components/BurgerMenu.svelte';
+	import Project from '$lib/components/Project.svelte';
   
 let isMobile = false;
 let showBurger = false;
-let scrollOnTopButton = false;
 
 
 //detect screen size
@@ -27,11 +23,7 @@ checkScreenSize();
 window.addEventListener('resize', checkScreenSize); 
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-        showBurger = true;
-    } else {
-        showBurger = false;
-    }
+    showBurger = window.scrollY > 100;
 });
 
 return () => {
@@ -44,40 +36,50 @@ return () => {
 <NavBar />
 
 
-<main class="h-screen bg-opacity-60 mt-40 flex flex-col items-start lg:flex-row">
+<main class="flex flex-col">
     
-    {#if !isMobile }
-        <div class="image flex justify-center items-center w-1/4 h-2/3">
-            <img class="w-3/4 max-h-[80%]" src="/blank-profile.png" alt="Picture_of_me">
-        </div>
-    {/if}
-    
+    <div class="First-screen h-screen bg-opacity-60 mt-40 flex flex-col items-start lg:flex-row">
 
-    <div class="flex flex-col w-3/4 justify-start gap-20">
-        
-        <div class="title flex flex-col items-center text-center h-fit mb-10">
-            <Heading tag="h1" class="mb-1" customSize="text-3xl font-bold md:text-4xl lg:text-5xl">
-                <Span>Thomas Koenig</Span>
-            </Heading>
-            <Heading tag="h2" class="mb-1" customSize="text-2xl font-bold md:text-2xl lg:text-3xl">
-                <Span>Étudiant en BUT informatique</Span>
-            </Heading>
-        </div>
-
-        {#if isMobile}
+        {#if !isMobile }
             <div class="image flex justify-center items-center w-1/4 h-2/3">
                 <img class="w-3/4 max-h-[80%]" src="/blank-profile.png" alt="Picture_of_me">
             </div>
         {/if}
-
-        <div class="flex justify-center self-start">
-            <AboutMe />
-        </div>
         
-        {#if showBurger}
-            <BurgerMenu />
-        {/if}
+
+        <div class="flex flex-col w-3/4 justify-start gap-20">
+            
+            <div class="title flex flex-col items-center text-center h-fit mb-10">
+                <Heading tag="h1" class="mb-1" customSize="text-3xl font-bold md:text-4xl lg:text-5xl">
+                    <Span>Thomas Koenig</Span>
+                </Heading>
+                <Heading tag="h2" class="mb-1" customSize="text-2xl font-bold md:text-2xl lg:text-3xl">
+                    <Span>Étudiant en BUT informatique</Span>
+                </Heading>
+            </div>
+
+            <!-- met l'image au milieu en version mobile -->
+            {#if isMobile}
+                <div class="image flex justify-center items-center w-1/4 h-2/3">
+                    <img class="w-3/4 max-h-[80%]" src="/blank-profile.png" alt="Picture_of_me">
+                </div>
+            {/if}
+
+            <div class="flex justify-center self-start">
+                <AboutMe />
+            </div>
+            
+            {#if showBurger}
+                <BurgerMenu />
+            {/if}
+        </div>
+    </div>
+    
+    <div id="projects" class="Project-section flex flex-col h-300 scroll-mt-20">
+        <Hr classHr="w-50% h-1 my-4 rounded-sm mb-5 dark:bg-orange bg-orange" />    
+        <Project />
     </div>
     
 
 </main>
+
