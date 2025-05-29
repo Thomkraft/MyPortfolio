fb2ee96fb8f731078eb2ab58d9868949d44637bf
thomas koenig <thomas.koenig@etu.univ-lyon1.fr>
2025-05-29 22:41:43 +0200

    Add hoover on buttons

--- src/lib/components/BurgerMenu.svelte
+++ src/lib/components/BurgerMenu.svelte
@@ -19,21 +19,25 @@
 
 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4" in:fade={{ duration: 250 }} out:fade={{ duration: 250 }}>
 
-    <!-- Bouton pour télécharger le CV  -->
-    <a 
-    href="/cv-koenig-thomas.pdf" 
-    download="cv-koenig-thomas.pdf"
-    class="fixed bottom-23 right-4 z-50 p-2 w-15 h-15 text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all"
+    <!-- Bouton pour télécharger le CV -->
+    <a
+            href="/cv-koenig-thomas.pdf"
+            download="cv-koenig-thomas.pdf"
+            title="Télécharger mon CV"
+            class="fixed bottom-24 right-4 z-50 p-2 w-15 h-15 rounded-full shadow-lg transition-all duration-300 ease-in-out transform hover:scale-110 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
     >
-        <DownloadSolid class="w-11 h-11 text-gray-800 dark:text-white" />
+        <DownloadSolid class="w-11 h-11 text-gray-800 dark:text-white hover:text-orange-500 transition-colors duration-300" />
     </a>
 
     <!-- Bouton pour remonter -->
-    <button 
-        class="fixed bottom-4 right-4 z-50 p-2 w-15 h-15 text-gray-500 bg-gray-200 rounded-full shadow-lg hover:bg-gray-300 focus:outline-none dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 transition-all"
-        on:click={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-        <ArrowUpOutline class="w-11 h-11 text-gray-800 dark:text-white" />
+    <button
+            title="Remonter en haut de la page"
+            class="fixed bottom-4 right-4 z-50 p-2 w-15 h-15 rounded-full shadow-lg transition-all duration-300 ease-in-out transform hover:scale-110 bg-gray-200 hover:bg-gray-300 dark:bg-gray-700 dark:hover:bg-gray-600"
+            on:click={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+    >
+        <ArrowUpOutline class="w-11 h-11 text-gray-800 dark:text-white hover:text-orange-500 transition-colors duration-300" />
     </button>
+
 
 </div>
 
